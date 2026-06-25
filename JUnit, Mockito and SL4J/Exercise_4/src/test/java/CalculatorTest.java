b5d18dba2f7f70d88e39eca2f1e1243d924ec6b0
jonithjo <jonithjo2005@gmail.com>
2026-06-25 14:53:20 +0530

    Exercise_4 added

---
+++ JUnit, Mockito and SL4J/Exercise_4/src/test/java/CalculatorTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setup Method Executed");
+        calculator = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Teardown Method Executed");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+
+        // Arrange
+        int a = 10;
+        int b = 20;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(30, result);
+    }
+}
